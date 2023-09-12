6ee5c65ed355e46071a8e3cc7d1d581b06638dae
Ian <goldfeld35@gmail.com>
2023-09-12 19:22:15 -0300

    Front-End Done 🎗

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function handleClick() {
+  window.open('https://github.com/iangs1', '_blank', 'noopener,noreferrer')
+}
