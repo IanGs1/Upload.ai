3e43dd72ab255862d97804fbdd975bd5ca58e472
Ian <goldfeld35@gmail.com>
2023-09-14 17:54:38 -0300

    Creating the AI Completition route 📭

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 
 import { getAllPrompts } from './routes/get-all-prompts';
 import { uploadVideo } from './routes/upload-video';
 import { createTranscription } from './routes/create-transcription';
+import { generateAICompletition } from './routes/generate-ai-completition';
 
 const app = fastify();
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(getAllPrompts);
 app.register(uploadVideo);
 app.register(createTranscription);
+app.register(generateAICompletition);
 
 app.listen({
     port: 3333
