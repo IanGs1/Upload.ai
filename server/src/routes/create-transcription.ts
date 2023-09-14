5018d7126d736c39391cb2e3a448264c8be20060
Ian <goldfeld35@gmail.com>
2023-09-13 23:38:58 -0300

    Updating the Transcription route 📭

--- server/src/routes/create-transcription.ts
+++ server/src/routes/create-transcription.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 
+import { createReadStream } from 'node:fs'
 import { z } from 'zod';
 
 import { prisma } from "../lib/prisma";
+import { openai } from '../lib/openai';
 
 export async function createTranscription(app: FastifyInstance) {
     app.post('/videos/:videoId/transcription', async (request, reply) => {
@@ -18,9 +20,35 @@ export async function createTranscription(app: FastifyInstance) {
 
         const { prompt } = bodySchema.parse(request.body);
 
-        return reply.status(201).send({
-            videoId,
+        const video = await prisma.video.findUniqueOrThrow({
+            where: {
+                id: videoId
+            }
+        })
+
+        const videoPath = video.path;
+        const audioReadStream = createReadStream(videoPath);
+
+        const response = await openai.audio.transcriptions.create({
+            file: audioReadStream,
+            model: 'whisper-1',
+            language: 'pt',
+            response_format: 'json',
+            temperature: 0,
             prompt
         })
+
+        const transcription = response.text;
+
+        await prisma.video.update({
+            where: {
+                id: video.id
+            },
+            data: {
+                transcription: transcription
+            }
+        })
+
+        return { transcription }
     })
 }
